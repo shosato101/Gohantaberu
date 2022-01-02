d203bb8dc7affd310dcf227a4537b6ccb196d0bb
s.sato <beetbood@gmail.com>
2022-01-02 15:57:01 +0900

    test

--- app.py
+++ app.py
@@ -17,10 +17,6 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('VesLcVYuxdpH+o/mcgIybSjqFA5IhxOXlloAT2MQXT2NEKCWV8LZ8i0ZDk37/N1pL0RD4nXvGtbFnX2vGkb4O2Kgk7AAOZDLyeqwowgZrNd7rFSUAABkBVx7L0eh6zWIj5HOpi7Br2HBLU4xI6iefAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('929e793e95704e31010b645aa0d3e31c')
 
-@app.route("/")
-def test():
-    return "OK"
-
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
