70973c4ea54392f4e40a632c0ec94f4c7f70d3a0
s.sato <beetbood@gmail.com>
2022-01-13 00:03:45 +0900

    test4

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 全角スペースを半角にするため
-    keyword = event.message.text
-    reccomend = pickup(keyword.replace("　", " "))
+ #   keyword = event.message.text
+    reccomend = pickup(event.message.text)
 
     line_bot_api.reply_message(
         event.reply_token,
